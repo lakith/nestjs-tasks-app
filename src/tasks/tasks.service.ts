fe40a475ac5422b9eb05144432d2c87ce03ab0c1
lmuthugala <lmuthugala@wiley.com>
2019-09-02 00:22:22 +0530

    adding task create function

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.modal';
+import { Task, TaskStatus } from './task.modal';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,16 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    addNewTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title, // same as title:title                        /****** In ES6 object litaral syntax is enhanced in this way
+            description, // same as description:description       *****/
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
     }
 }
